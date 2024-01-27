01f35be6ea06aaf9cf094fb54bb5ee26621ef8b0
yurong <wyrrong@gmail.com>
2024-01-27 11:38:23 +0800

    test1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D playerRB;
+    private bool isGrounded;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    public void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector2 moveDirection = new Vector2(horizontalInput, 0f);
+        playerRB.velocity = new Vector2(moveDirection.x * moveSpeed, playerRB.velocity.y);
+    }
+
+    public void Jump()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            PlayerData.currentDepress += 20f;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
