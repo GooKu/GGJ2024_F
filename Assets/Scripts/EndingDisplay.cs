1149ca00384111c7bcdb615d9dd4269c5fe23657
yurong <wyrrong@gmail.com>
2024-01-27 18:57:12 +0800

    ending display func edit

--- Assets/Scripts/EndingDisplay.cs
+++ Assets/Scripts/EndingDisplay.cs
@@ -5,31 +5,15 @@ using UnityEngine;
 public class EndingDisplay : MonoBehaviour
 {
     public GameObject FailEndUI, GoodEndUI, NormalEnd;
-    private int depressScore, screamScore;
 
     public void EndingUIDisplay(int depressScore)
     {
-        if(depressScore > 0){
-            NormalEnd.SetActive(true);
-
-            FailEndUI.SetActive(false);
-            GoodEndUI.SetActive(false);
-        }
-
-        else if (depressScore >= 100)
-        {
-            FailEndUI.SetActive(true);
-
-            GoodEndUI.SetActive(false);
-            NormalEnd.SetActive(false);
-        }
-
-        else if(depressScore <= 0)
-        {
-            GoodEndUI.SetActive(true);
-
-            FailEndUI.SetActive(false);
-            NormalEnd.SetActive(false);
-        }
+        NormalEnd.SetActive(depressScore > 0);
+        FailEndUI.SetActive(depressScore >= 100);
+        GoodEndUI.SetActive(depressScore <= 0);
+        
+        FailEndUI.SetActive(FailEndUI.activeSelf && !(depressScore >= 100));
+        GoodEndUI.SetActive(GoodEndUI.activeSelf && !(depressScore <= 0));
+        NormalEnd.SetActive(NormalEnd.activeSelf && !FailEndUI.activeSelf && !GoodEndUI.activeSelf);
     }
 }
