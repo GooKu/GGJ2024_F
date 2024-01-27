35e050a91045b0ce5d0b77410d07feff93c01bdf
yurong <wyrrong@gmail.com>
2024-01-27 15:16:32 +0800

    Scripts/ScreamItem.cs Scripts/PlayerData.cs

--- Assets/Scripts/ScreamItem.cs
+++ Assets/Scripts/ScreamItem.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class ScreamItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if( collision.CompareTag("Player") && collision.GetType().ToString() == "UnityEngine.CapsuleCollider2D")
+        {
+            GameManger.Instance.GetPlayerData().currentScream += 20;
+            Destroy(gameObject);
+        }
     }
 }
