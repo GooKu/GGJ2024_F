8cebf4419b8598eec28b865d01ff2276a420d8b9
Goo <kuwoods@hotmail.com>
2024-01-28 10:26:31 +0800

    Implement EndPoint

---
+++ Assets/Scripts/EndPoint.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerMovement>().enabled = false;
+            var endDisplay = GameManger.FindFirstObjectByType<EndingDisplay>();
+            endDisplay.EndingUIDisplay(GameManger.Instance.GetDepress());
+        }
+    }
+}
